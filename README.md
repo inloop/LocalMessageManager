b7e8f8a78710feff19f1c7fd3980a6e5a2ea7c05
Daniel Novak <novak@inloop.eu>
2016-10-06 17:30:35 +0200

    Basic project skeleton

---
+++ README.md
@@ -0,0 +1,16 @@
+LocalMessageManager
+================
+
+TODO:
+
+How to implement
+--------
+
+
+Download
+--------
+
+Grab via Gradle:
+```groovy
+compile 'eu.inloop:localmessagemanager:0.1.0'
+```
---
+++ sample/src/main/java/eu/inloop/localmessagemanager/sample/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package eu.inloop.localmessagemanager.sample.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import eu.inloop.localmessagemanager.sample.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
